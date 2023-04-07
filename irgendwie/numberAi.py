5665af77bf5c258f2631a6f0ee25139559302cc5
Antonus2 <113021198+Antonus2@users.noreply.github.com>
2023-04-07 21:16:37 +0200

    ...

--- irgendwie/numberAi.py
+++ irgendwie/numberAi.py
@@ -4,10 +4,19 @@ import sklearn.linear_model as lm
 import matplotlib.pyplot as plt
 
 
-with open('C:\Users\imene\Desktop\numberAI\ai_tests\irgendwie\data\train-images.idx3-ubyte', 'rb') as f:
+with open('data\\train-labels.idx1-ubyte', 'rb') as f:
     magic_number = struct.unpack('>4B', f.read(4))
     num_labels = struct.unpack('>I', f.read(4))[0]
-label_data = np.fromfile(f, dtype=np.uint8)
-label_data = label_data.reshape(num_labels)
+    label_data = np.fromfile(f, dtype=np.uint8)
+    label_data = label_data.reshape(num_labels)
 
+with open('data\\train-images.idx3-ubyte', 'rb') as f:
+    magic_number = struct.unpack('>4B', f.read(4))
+    num_images = struct.unpack('>I', f.read(4))[0]
+    num_rows = struct.unpack('>I', f.read(4))[0]
+    num_cols = struct.unpack('>I', f.read(4))[0]
+    image_data = np.fromfile(f, dtype=np.uint8)
+    image_data = image_data.reshape(num_images, num_rows, num_cols)
+
+print(image_data)
 print(label_data)
