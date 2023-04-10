6ce5e109251608246532efe871f16ecee6cbff39
Antonus2 <113021198+Antonus2@users.noreply.github.com>
2023-04-10 20:09:54 +0200

    ...

--- mandel/brot.py
+++ mandel/brot.py
@@ -1,23 +1,22 @@
-import pycuda.autoinit
-import pycuda.driver as cuda
 import numpy as np
-from pycuda.compiler import SourceModule
 import pygame
+import pyopencl as cl
 
 WIDTH, HEIGHT = 800, 800
 MAX_ITERATIONS = 1000
 
 kernel_code = """
-__global__ void mandelbrot(int *image, int width, int height, float zoom, int max_iterations) {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
+__kernel void mandelbrot(__global int *image, int width, int height, float zoom, int max_iterations,
+                         float offsetX, float offsetY) {
+    int x = get_global_id(0);
+    int y = get_global_id(1);
 
     if (x >= width || y >= height) {
         return;
     }
 
-    float real = (x - width / 2.0) * 4.0 / (width * zoom);
-    float imag = (y - height / 2.0) * 4.0 / (height * zoom);
+    float real = (x - width / 2.0f) * 4.0f / (width * zoom) + offsetX;
+    float imag = (y - height / 2.0f) * 4.0f / (height * zoom) + offsetY;
     float cReal = real;
     float cImag = imag;
 
@@ -25,7 +24,7 @@ __global__ void mandelbrot(int *image, int width, int height, float zoom, int ma
     for (value = 0; value < max_iterations; ++value) {
         float r2 = real * real;
         float i2 = imag * imag;
-        if (r2 + i2 > 4.0) {
+        if (r2 + i2 > 4.0f) {
             break;
         }
 
@@ -36,23 +35,40 @@ __global__ void mandelbrot(int *image, int width, int height, float zoom, int ma
         imag = newImag;
     }
 
-    image[y * width + x] = value;
+    float color = value == max_iterations ? 0 : value;
+    int r = (int)(color / max_iterations * 255);
+    int g = (int)(color / (max_iterations / 2.0f) * 255);
+    int b = (int)(color / (max_iterations / 4.0f) * 255);
+
+    image[y * width + x] = (r << 16) | (g << 8) | b;
 }
 """
 
-mandelbrot_kernel = SourceModule(kernel_code).get_function("mandelbrot")
 
-def generate_mandelbrot_image(width, height, zoom, max_iterations):
-    image_gpu = cuda.mem_alloc(width * height * np.dtype(np.int32).itemsize)
-    image_cpu = np.empty((height, width), dtype=np.int32)
+class MandelbrotGenerator:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.ctx = cl.create_some_context()
+        print(f"Using device: {self.ctx.devices[0].name}")
+        self.queue = cl.CommandQueue(self.ctx)
+        self.prg = cl.Program(self.ctx, kernel_code).build()
+        self.mandelbrot_kernel = self.prg.mandelbrot
 
-    grid = (width // 32, height // 32, 1)
-    block = (32, 32, 1)
+    def generate_mandelbrot_image(self, zoom, max_iterations, offsetX, offsetY):
+        image_gpu = cl.Buffer(self.ctx, cl.mem_flags.WRITE_ONLY, self.width * self.height * np.dtype(np.int32).itemsize)
+        image_cpu = np.empty((self.height, self.width), dtype=np.int32)
 
-    mandelbrot_kernel(image_gpu, np.int32(width), np.int32(height), np.float32(zoom), np.int32(max_iterations), grid=grid, block=block)
-    cuda.memcpy_dtoh(image_cpu, image_gpu)
+        global_work_size = (self.width, self.height)
+        local_work_size = (32, 32)
 
-    return image_cpu
+        self.mandelbrot_kernel(self.queue, global_work_size, local_work_size, image_gpu, np.int32(self.width),
+                               np.int32(self.height), np.float32(zoom),
+                               np.int32(max_iterations), np.float32(offsetX), np.float32(offsetY))
+        cl.enqueue_copy(self.queue, image_cpu, image_gpu)
+
+        return image_cpu
+
 
 def main():
     pygame.init()
@@ -60,6 +76,11 @@ def main():
     clock = pygame.time.Clock()
     zoom = 1.0
     zoom_speed = 1.01
+    offsetX = 0.0
+    offsetY = 0.0
+    move_speed = 0.05
+
+    mandelbrot_generator = MandelbrotGenerator(WIDTH, HEIGHT)
 
     running = True
     while running:
@@ -67,13 +88,28 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        image_cpu = generate_mandelbrot_image(WIDTH, HEIGHT, zoom, MAX_ITERATIONS)
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            offsetX -= move_speed / zoom
+        if keys[pygame.K_s]:
+            offsetX += move_speed / zoom
+        if keys[pygame.K_a]:
+            offsetY -= move_speed / zoom
+        if keys[pygame.K_d]:
+            offsetY += move_speed / zoom
+        if keys[pygame.K_UP]:
+            zoom *= zoom_speed
+        if keys[pygame.K_DOWN]:
+            zoom /= zoom_speed
+
+        image_cpu = mandelbrot_generator.generate_mandelbrot_image(zoom, MAX_ITERATIONS, offsetX, offsetY)
         pygame.surfarray.blit_array(screen, image_cpu)
         pygame.display.flip()
         clock.tick(30)
-        zoom *= zoom_speed
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
