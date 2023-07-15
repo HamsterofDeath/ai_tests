8cc729d4bd215461c56f5a2941887efda52427b7
hamsterofdeath <h-star@gmx.de>
2023-07-15 12:52:54 +0200

    ...

--- karol2/src/main/scala/SmaBot.scala
+++ karol2/src/main/scala/SmaBot.scala
@@ -1,127 +1,137 @@
 import java.awt.event.{ActionEvent, ActionListener}
 import java.awt.image.BufferedImage
 import java.awt.{BorderLayout, Color, Graphics2D}
-import javax.swing.{ImageIcon, JFrame, JLabel, JPanel, SwingUtilities, Timer, WindowConstants}
+import javax.swing.*
+import scala.collection.mutable.ArrayBuffer
 
-// Define the directions
-sealed trait Direction
-case object Up extends Direction
-case object Down extends Direction
-case object Left extends Direction
-case object Right extends Direction
-
-// Define the position
-class Position(var x: Int, var y: Int)
-
-class Player(var position: Position, var direction: Direction) {
-  def move(): Unit = direction match {
-    case Up => position.y -= 1
-    case Down => position.y += 1
-    case Left => position.x -= 1
-    case Right => position.x += 1
-  }
-
-  def turnLeft(): Unit = direction match {
-    case Up => direction = Left
-    case Down => direction = Right
-    case Left => direction = Down
-    case Right => direction = Up
-  }
-
-  def turnRight(): Unit = direction match {
-    case Up => direction = Right
-    case Down => direction = Left
-    case Left => direction = Up
-    case Right => direction = Down
-  }
-}
-
-
-sealed trait Move {
-  def doIt(player: Player, field: GameField):Unit
-}
-case object TurnLeft extends Move {
-  override def doIt(player: Player, field: GameField): Unit = player.turnLeft()
-}
-case object TurnRight extends Move{
-  override def doIt(player: Player, field: GameField): Unit = player.turnRight()
-}
-
-case object MoveForward extends Move {
-  override def doIt(player: Player, field: GameField): Unit = {
-    val isFreeAndInside = player.direction match {
-      case Up    => player.position.y > 0 && field.grid(player.position.y - 1)(player.position.x) == 0
-      case Down  => player.position.y < field.grid.length - 1 && field.grid(player.position.y + 1)(player.position.x) == 0
-      case Left  => player.position.x > 0 && field.grid(player.position.y)(player.position.x - 1) == 0
-      case Right => player.position.x < field.grid(0).length - 1 && field.grid(player.position.y)(player.position.x + 1) == 0
-    }
-
-    if (isFreeAndInside) player.move()
-  }
-}
-
-
-case object PlaceBlock extends Move{
-  override def doIt(player: Player, field: GameField): Unit = field.placeBlock(player)
-}
-
-case object RemoveBlock extends Move{
-  override def doIt(player: Player, field: GameField): Unit = field.removeBlock(player)
-}
-
-case object Idle extends Move {
-  override def doIt(player: Player, field: GameField): Unit = {}
-}
-
-class GameField(val grid: Array[Array[Int]])  {
-
-
-  def scan(player: Player): Int = {
-    // return distance to the next block or the end of the field
-    player.direction match {
-      case Up =>
-        (0 until player.position.y).findLast(y => grid(y)(player.position.x) == 1)
-          .map(blockY => player.position.y - blockY - 1).getOrElse(player.position.y)
-
-      case Down =>
-        (player.position.y until grid.length).find(y => grid(y)(player.position.x) == 1)
-          .map(blockY => blockY - player.position.y - 1).getOrElse(grid.length - player.position.y - 1)
-
-      case Left =>
-        (0 until player.position.x).findLast(x => grid(player.position.y)(x) == 1)
-          .map(blockX => player.position.x - blockX - 1).getOrElse(player.position.x)
-
-      case Right =>
-        (player.position.x until grid(0).length).find(x => grid(player.position.y)(x) == 1)
-          .map(blockX => blockX - player.position.x - 1).getOrElse(grid(0).length - player.position.x - 1)
-    }
-  }
-  def placeBlock(player: Player): Unit = {
-    // Implement logic to place a block in the grid in front of the player's location
-    player.direction match {
-      case Up if player.position.y > 0 => grid(player.position.y - 1)(player.position.x) = 1
-      case Down if player.position.y < grid.length - 1 => grid(player.position.y + 1)(player.position.x) = 1
-      case Left if player.position.x > 0 => grid(player.position.y)(player.position.x - 1) = 1
-      case Right if player.position.x < grid(0).length - 1 => grid(player.position.y)(player.position.x + 1) = 1
-      case _ =>
-    }
-  }
-
-  def removeBlock(player: Player): Unit = {
-    // Implement logic to remove a block from the grid in front of the player's location
-    player.direction match {
-      case Up if player.position.y > 0 && grid(player.position.y - 1)(player.position.x) == 1 => grid(player.position.y - 1)(player.position.x) = 0
-      case Down if player.position.y < grid.length - 1 && grid(player.position.y + 1)(player.position.x) == 1 => grid(player.position.y + 1)(player.position.x) = 0
-      case Left if player.position.x > 0 && grid(player.position.y)(player.position.x - 1) == 1 => grid(player.position.y)(player.position.x - 1) = 0
-      case Right if player.position.x < grid(0).length - 1 && grid(player.position.y)(player.position.x + 1) == 1 => grid(player.position.y)(player.position.x + 1) = 0
-      case _ =>
-    }
-  }
-}
 
 object SmaBot {
+
+  // Define the directions
+  sealed trait Direction
+
+  case object Up extends Direction
+
+  case object Down extends Direction
+
+  case object Left extends Direction
+
+  case object Right extends Direction
+
+  // Define the position
+  class Position(var x: Int, var y: Int)
+
+  class Player(var position: Position, var direction: Direction) {
+    def move(): Unit = direction match {
+      case Up => position.y -= 1
+      case Down => position.y += 1
+      case Left => position.x -= 1
+      case Right => position.x += 1
+    }
+
+    def turnLeft(): Unit = direction match {
+      case Up => direction = Left
+      case Down => direction = Right
+      case Left => direction = Down
+      case Right => direction = Up
+    }
+
+    def turnRight(): Unit = direction match {
+      case Up => direction = Right
+      case Down => direction = Left
+      case Left => direction = Up
+      case Right => direction = Down
+    }
+  }
+
+
+  sealed trait Move {
+    def doIt(player: Player, field: GameField): Unit
+  }
+
+  case object TurnLeft extends Move {
+    override def doIt(player: Player, field: GameField): Unit = player.turnLeft()
+  }
+
+  case object TurnRight extends Move {
+    override def doIt(player: Player, field: GameField): Unit = player.turnRight()
+  }
+
+  case object MoveForward extends Move {
+    override def doIt(player: Player, field: GameField): Unit = {
+      val isFreeAndInside = player.direction match {
+        case Up => player.position.y > 0 && field.grid(player.position.y - 1)(player.position.x) == 0
+        case Down => player.position.y < field.grid.length - 1 && field.grid(player.position.y + 1)(player.position.x) == 0
+        case Left => player.position.x > 0 && field.grid(player.position.y)(player.position.x - 1) == 0
+        case Right => player.position.x < field.grid(0).length - 1 && field.grid(player.position.y)(player.position.x + 1) == 0
+      }
+
+      if (isFreeAndInside) player.move()
+    }
+  }
+
+
+  case object PlaceBlock extends Move {
+    override def doIt(player: Player, field: GameField): Unit = field.placeBlock(player)
+  }
+
+  case object RemoveBlock extends Move {
+    override def doIt(player: Player, field: GameField): Unit = field.removeBlock(player)
+  }
+
+  case object Idle extends Move {
+    override def doIt(player: Player, field: GameField): Unit = {}
+  }
+
+  class GameField(val grid: Array[Array[Int]]) {
+
+
+    def scan(player: Player): Int = {
+      // return distance to the next block or the end of the field
+      player.direction match {
+        case Up =>
+          (0 until player.position.y).findLast(y => grid(y)(player.position.x) == 1)
+            .map(blockY => player.position.y - blockY - 1).getOrElse(player.position.y)
+
+        case Down =>
+          (player.position.y until grid.length).find(y => grid(y)(player.position.x) == 1)
+            .map(blockY => blockY - player.position.y - 1).getOrElse(grid.length - player.position.y - 1)
+
+        case Left =>
+          (0 until player.position.x).findLast(x => grid(player.position.y)(x) == 1)
+            .map(blockX => player.position.x - blockX - 1).getOrElse(player.position.x)
+
+        case Right =>
+          (player.position.x until grid(0).length).find(x => grid(player.position.y)(x) == 1)
+            .map(blockX => blockX - player.position.x - 1).getOrElse(grid(0).length - player.position.x - 1)
+      }
+    }
+
+    def placeBlock(player: Player): Unit = {
+      // Implement logic to place a block in the grid in front of the player's location
+      player.direction match {
+        case Up if player.position.y > 0 => grid(player.position.y - 1)(player.position.x) = 1
+        case Down if player.position.y < grid.length - 1 => grid(player.position.y + 1)(player.position.x) = 1
+        case Left if player.position.x > 0 => grid(player.position.y)(player.position.x - 1) = 1
+        case Right if player.position.x < grid(0).length - 1 => grid(player.position.y)(player.position.x + 1) = 1
+        case _ =>
+      }
+    }
+
+    def removeBlock(player: Player): Unit = {
+      // Implement logic to remove a block from the grid in front of the player's location
+      player.direction match {
+        case Up if player.position.y > 0 && grid(player.position.y - 1)(player.position.x) == 1 => grid(player.position.y - 1)(player.position.x) = 0
+        case Down if player.position.y < grid.length - 1 && grid(player.position.y + 1)(player.position.x) == 1 => grid(player.position.y + 1)(player.position.x) = 0
+        case Left if player.position.x > 0 && grid(player.position.y)(player.position.x - 1) == 1 => grid(player.position.y)(player.position.x - 1) = 0
+        case Right if player.position.x < grid(0).length - 1 && grid(player.position.y)(player.position.x + 1) == 1 => grid(player.position.y)(player.position.x + 1) = 0
+        case _ =>
+      }
+    }
+  }
+
   private val gridSize = 20
-  private val gameField = new GameField(Array.fill(gridSize,gridSize)(0))
+  private val gameField = new GameField(Array.fill(gridSize, gridSize)(0))
   private val player = new Player(Position(0, 0), Up)
   private val blockSize = 20
 
@@ -169,14 +179,14 @@ object SmaBot {
     image
   }
 
-  private val timer = new Timer(1000, new ActionListener() {
+  private val timer = new Timer(Logic.delay, new ActionListener() {
     def actionPerformed(e: ActionEvent): Unit = {
       Logic.getNextAction(gameField.scan(player)).doIt(player, gameField)
       label.setIcon(new ImageIcon(renderGrid(gameField.grid)))
     }
   })
 
-  def main(args: Array[String]): Unit = {
+  def run(): Unit = {
     SwingUtilities.invokeLater(new Runnable {
       def run(): Unit = {
         label.setIcon(new ImageIcon(renderGrid(gameField.grid)))
@@ -193,7 +203,40 @@ object SmaBot {
 
 object Logic {
 
-  def getNextAction(distanceToNextBlock:Int) = {
-    TurnLeft
+  import SmaBot.*
+
+  val delay = 1
+
+  def main(args: Array[String]): Unit = {
+    SmaBot.run()
+  }
+
+  val befehle = ArrayBuffer.empty[Move]
+
+  def getNextAction(distanceToNextBlock: Int): Move = {
+    if (befehle.nonEmpty) {
+      val befehlAusListe = befehle.head
+      befehle.remove(0)
+      return befehlAusListe
+    }
+
+    if (distanceToNextBlock > 0) {
+      befehle += MoveForward
+      befehle += TurnLeft
+      befehle += TurnLeft
+      befehle += PlaceBlock
+      befehle += TurnRight
+      befehle += TurnRight
+      return Idle
+    }
+    else {
+      return TurnRight
+    }
+
   }
 }
+//TurnLeft
+//    TurnRight
+//    MoveForward
+//    PlaceBlock
+//    RemoveBlock
